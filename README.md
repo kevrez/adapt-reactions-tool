dd0b3fdccd212ecd6aea9af12760a8108c69055e
Kevin <kreznicek@me.com>
2021-07-20 18:42:00 -0400

    added documentation, minor development

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# adapt-reactions-tool
+# ADAPT PT/RC Reactions Tool - Documentation
 This script prints reactions outputted from ADAPT in a DL-SDL-LL format, grouped by support instead of ADAPT's default, by load case.
+It allows the user to copy reactions from ADAPT's output quickly, insted of having to manually extract each reaction.
+
+This script is intended for the structural engineer designing concrete beams in ADAPT, to aid in laying out reactions from ADAPT's output. It is assumed that you are working with a Windows PC.
+
+## Disclosure
+Always verify your results. It is up to the design engineer and engineer of record to verify the integrity of their design. I do NOT take responsibility for any element of the design related to the use of this script. It is merely intended to aid you in the design process.
+
+## Installation Instructions
+
+1. Install Python 3 if you don't already. Refer to python.org for more info.
+2. Download ***adapt_reactions_tool_xls.py***
+3. Use pip to install *xlrd* using the following command in the Python 3 interpreter:
+
+        pip3 install xlrd
+    *xlrd* is a Python library used to parse legacy .xls files.
+
+You're good to go!
+
+## Use
+
+### Output a Report from your ADAPT Run
+Once your ADAPT run is complete, output a design report for your run with 'Skip Live Load' disabled. Your report must contain the *'Moments, Shears, and Reactions'* tables under *'Tabular Reports - Compact'*. 
+
+Ensure that the 'Create Optional XLS Report' option is checked. ***adapt_reactions_tool_xls.py*** uses this file to output its results. Save this .xls file and copy its full path:
+
+1. Hold Shift and right-click the .xls file within Windows Explorer
+2. Click **'Copy as Path'**
+
+### Open the Script
+Simply run the *adapt_reactions_tool_xls.py* file using Python. You can either double click the file if Python is your default program to run *.py* files, or right click on the file and navigate to *Open With... -> Python*. Alternatively, run the script from your IDE of choice. 
+
+### Output the Reactions
+Almost there!
+
+***adapt_reactions_tool_xls.py*** will prompt you for a path to an Excel sheet with reactions. Paste the path we copied before opening the script and hit Enter. The script will print the reactions from ADAPT grouped by support, and ordered by load case: 
+- DL
+- SDL
+- LL
+
+Note: ***adapt_reactions_tool_xls.py*** does not yet support ADAPT's X load case.
+
+You can input as many spreadsheets as you like. To exit the program, input *'quit'*, *'exit'*, or *'close'*.'
+
--- adapt_reactions_tool_xls.py
+++ adapt_reactions_tool_xls.py
@@ -1,6 +1,5 @@
 import xlrd
 
-# save_path = r'"/Users/kevin/Dropbox/Python/Projects/ADAPT Reactions Tool/rxns.xls"'
 RXN_PAGE = '(10)'
 
 FIRST_RXN_ROW = 6
@@ -10,49 +9,55 @@ RXN_VAL_COL = 3
 DEAD_LOAD_TEXT = 'DL'
 SDL_LOAD_TEXT = 'SDL'
 LL_LOAD_TEXT = 'LL'
+MAX_SHEET_ROW = 50
 
 if __name__ == '__main__':
     DL = []
     SDL = []
     LL = []
 
-    save_path = input('\nInput Excel Save Path. Note: Can include ":\n').replace('"', "")
-    excel_report = xlrd.open_workbook(save_path)
-    ws = excel_report.sheet_by_name(RXN_PAGE)
+    while True:
+        save_path = input('\nInput Excel Save Path. Note: Can include quotation marks ". Enter "exit" to end:\n').replace('"', '')
+        if save_path.lower() in ('quit', 'exit', 'close', 'end'):
+            break      
+        
+        seen_LL = False
+        cur_row = FIRST_RXN_ROW
 
-    seen_LL = False
-    cur_row = FIRST_RXN_ROW
+        try:
+            excel_report = xlrd.open_workbook(save_path)
+            ws = excel_report.sheet_by_name(RXN_PAGE)
 
-    try:
-        while not seen_LL or ws.cell(cur_row, RXN_TYPE_COL).value:
+            while not seen_LL or ws.cell(cur_row, RXN_TYPE_COL).value:
+                if ws.cell(cur_row, RXN_TYPE_COL).value == DEAD_LOAD_TEXT:
+                    DL.append(ws.cell(cur_row, RXN_VAL_COL).value)
+                elif ws.cell(cur_row, RXN_TYPE_COL).value == SDL_LOAD_TEXT:
+                    SDL.append(ws.cell(cur_row, RXN_VAL_COL).value)
+                elif ws.cell(cur_row, RXN_TYPE_COL).value == LL_LOAD_TEXT:
+                    LL.append(ws.cell(cur_row, RXN_VAL_COL).value)
+                    if not seen_LL:
+                        seen_LL = True
+                cur_row +=1
 
-            if ws.cell(cur_row, RXN_TYPE_COL).value == DEAD_LOAD_TEXT:
-                DL.append(ws.cell(cur_row, RXN_VAL_COL).value)
+                if cur_row > MAX_SHEET_ROW:
+                    break
 
-            elif ws.cell(cur_row, RXN_TYPE_COL).value == SDL_LOAD_TEXT:
-                SDL.append(ws.cell(cur_row, RXN_VAL_COL).value)
+        except IndexError:
+            print(f'\nReached end of file at row {cur_row}.')
 
-            elif ws.cell(cur_row, RXN_TYPE_COL).value == LL_LOAD_TEXT:
-                LL.append(ws.cell(cur_row, RXN_VAL_COL).value)
-                if not seen_LL:
-                    seen_LL = True
-            cur_row +=1
+        except FileNotFoundError:
+            print('File not found.')
+            continue
 
-            if cur_row > 100:
-                break
-
-    except IndexError:
-        print(f'\nReached end of file at row {cur_row}')
-
-    if len(DL) == 0 or not (len(DL) == len(SDL) == len(LL)):
-        print('There was an error with reading the reactions.')
-    else:
-        print("\nFormat:\nDL\nSDL\nLL\n")
-        for i in range(len(DL)):
-            print(f'Support {i+1}:')
-            print(f'{DL[i]} k')
-            print(f'{SDL[i]} k')
-            print(f'{LL[i]} k')
-            print()
-    
-    input('SUCCESS\nPress Enter to exit:\n')
+        if len(DL) == 0 or not (len(DL) == len(SDL) == len(LL)):
+            print("Reactions not found. Ensure that the "
+                + "'Moments, Shears, and Reactions' section is "
+                + "included in the report.")
+        else:
+            print("\nFormat:\nDL\nSDL\nLL\n")
+            for i in range(len(DL)):
+                print(f'Support {i+1}:')
+                print(f'DL = {DL[i]} k')
+                print(f'SDL = {SDL[i]} k')
+                print(f'LL = {LL[i]} k')
+        
